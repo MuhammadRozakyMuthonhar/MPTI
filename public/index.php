759581bec9a27d06593826f522f50ea4e5a1d332
Muhammad Rozaky Muthohar <mrmcraftbuild@gmail.com>
2025-07-16 21:42:02 +0700

    Fix: proper bootstrap of Laravel app in index.php

--- public/index.php
+++ public/index.php
@@ -1,19 +1,22 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Maintenance mode check
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-echo "Laravel started";
-
-// Register the Composer autoloader...
+// Autoload dependencies
 require __DIR__.'/../vendor/autoload.php';
 
+// ✅ Bootstrap Laravel application (membuat $app)
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Jalankan kernel Laravel
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
